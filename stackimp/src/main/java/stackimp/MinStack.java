0df7fbaa43ac80a069fe46f4c3192c15d4be865b
Pruthvi Iswaryam <Pruthvi Iswaryam@192.168.1.39>
2022-07-20 16:37:31 +0530

    Adding push and display methods

--- stackimp/src/main/java/stackimp/MinStack.java
+++ stackimp/src/main/java/stackimp/MinStack.java
@@ -1,5 +1,48 @@
 package stackimp;
 
-public class MinStack {
+import java.util.EmptyStackException;
 
+public class MinStack {
+	
+	private static Node top;
+	private static int size;
+
+
+
+	public void push(int data)
+	{
+		Node newNode = new Node(data);
+		
+		newNode.setNext(top);
+		top = newNode;
+		size++;
+		
+	}
+
+		
+
+	public static void display() {
+		Node node = top;
+		
+		while(node!=null)
+		{
+			System.out.print(node.getData()+" ");
+			node = node.getNext();
+		}
+
+	}
+	
+     public static void main(String[] args) {
+		
+    	 MinStack stack = new MinStack();
+    	 
+			
+			  stack.push(10); stack.push(20); stack.push(30);
+			 
+			  stack.display();
+    	 
+    	 
+    	
+	}
+	
 }
